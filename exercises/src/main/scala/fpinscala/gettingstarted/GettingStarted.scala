50258414adc72b17f5cae7f02cac1820e1d6d550
Leonhard Riedisser <leo008180@googlemail.com>
2018-08-03 19:03:12 +0200

    getting started

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -1,5 +1,6 @@
 package fpinscala.gettingstarted
 
+import scala.annotation.tailrec
 // A comment!
 /* Another comment */
 /** A documentation comment */
@@ -13,15 +14,19 @@ object MyModule {
     msg.format(x, abs(x))
   }
 
-  def main(args: Array[String]): Unit =
+  private def formatAbs2(x: Int) = s"The absolute value of $x is ${abs(x)}"
+
+  def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
+    println(formatAbs2(-42))
+  }
 
   // A definition of factorial, using a local, tail recursive function
   def factorial(n: Int): Int = {
-    @annotation.tailrec
+    @tailrec
     def go(n: Int, acc: Int): Int =
       if (n <= 0) acc
-      else go(n-1, n*acc)
+      else go(n - 1, n * acc)
 
     go(n, 1)
   }
@@ -30,13 +35,23 @@ object MyModule {
   def factorial2(n: Int): Int = {
     var acc = 1
     var i = n
-    while (i > 0) { acc *= i; i -= 1 }
+    while (i > 0) {
+      acc *= i
+      i -= 1
+    }
     acc
   }
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    @tailrec
+    def go(n: Int, prev: Int, cur: Int): Int =
+      if (n == 0) prev
+      else go(n - 1, cur, prev + cur)
+
+    go(n, 0, 1)
+  }
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
@@ -79,6 +94,7 @@ object TestFib {
 // convenient to have syntax for constructing a function
 // *without* having to give it a name
 object AnonymousFunctions {
+
   import MyModule._
 
   // Some examples of anonymous functions:
@@ -86,10 +102,13 @@ object AnonymousFunctions {
     println(formatResult("absolute value", -42, abs))
     println(formatResult("factorial", 7, factorial))
     println(formatResult("increment", 7, (x: Int) => x + 1))
-    println(formatResult("increment2", 7, (x) => x + 1))
+    println(formatResult("increment2", 7, x => x + 1))
     println(formatResult("increment3", 7, x => x + 1))
     println(formatResult("increment4", 7, _ + 1))
-    println(formatResult("increment5", 7, x => { val r = x + 1; r }))
+    println(formatResult("increment5", 7, x => {
+      val r = x + 1;
+      r
+    }))
   }
 }
 
@@ -101,65 +120,129 @@ object MonomorphicBinarySearch {
   // Ideally, we could generalize this to work for any `Array` type,
   // so long as we have some way of comparing elements of the `Array`
   def binarySearch(ds: Array[Double], key: Double): Int = {
-    @annotation.tailrec
+    @tailrec
     def go(low: Int, mid: Int, high: Int): Int = {
       if (low > high) -mid - 1
       else {
         val mid2 = (low + high) / 2
         val d = ds(mid2) // We index into an array using the same
-                         // syntax as function application
+        // syntax as function application
         if (d == key) mid2
-        else if (d > key) go(low, mid2, mid2-1)
+        else if (d > key) go(low, mid2, mid2 - 1)
         else go(mid2 + 1, mid2, high)
       }
     }
+
     go(0, 0, ds.length - 1)
+  }
+
+  // First, a findFirst, specialized to `String`.
+  // Ideally, we could generalize this to work for any `Array` type.
+  def findFirst(ss: Array[String], key: String): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+    // If `n` is past the end of the array, return `-1`
+    // indicating the key doesn't exist in the array.
+      if (n >= ss.length) -1
+      // `ss(n)` extracts the n'th element of the array `ss`.
+      // If the element at `n` is equal to the key, return `n`
+      // indicating that the element appears in the array at that index.
+      else if (ss(n) == key) n
+      else loop(n + 1) // Otherwise increment `n` and keep looking.
+    // Start the loop at the first element of the array.
+    loop(0)
   }
 
 }
 
 object PolymorphicFunctions {
 
+  // Here's a polymorphic version of `findFirst`, parameterized on
+  // a function for testing whether an `A` is the element we want to find.
+  // Instead of hard-coding `String`, we take a type `A` as a parameter.
+  // And instead of hard-coding an equality check for a given key,
+  // we take a function with which to test each element of the array.
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+      if (n >= as.length) -1
+      // If the function `p` matches the current element,
+      // we've found a match and we return its index in the array.
+      else if (p(as(n))) n
+      else loop(n + 1)
+
+    loop(0)
+  }
+
   // Here's a polymorphic version of `binarySearch`, parameterized on
   // a function for testing whether an `A` is greater than another `A`.
-  def binarySearch[A](as: Array[A], key: A, gt: (A,A) => Boolean): Int = {
-    @annotation.tailrec
+  def binarySearch[A](as: Array[A], key: A, gt: (A, A) => Boolean): Int = {
+    @tailrec
     def go(low: Int, mid: Int, high: Int): Int = {
       if (low > high) -mid - 1
       else {
         val mid2 = (low + high) / 2
         val a = as(mid2)
         val greater = gt(a, key)
-        if (!greater && !gt(key,a)) mid2
-        else if (greater) go(low, mid2, mid2-1)
+        if (!greater && !gt(key, a)) mid2
+        else if (greater) go(low, mid2, mid2 - 1)
         else go(mid2 + 1, mid2, high)
       }
     }
+
     go(0, 0, as.length - 1)
   }
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    def go(i: Int): Boolean =
+      if (i == as.length - 2) gt(as(i), as(as.length - 1))
+      else if (gt(as(i), as(i + 1))) go(i + 1)
+      else false
+
+    go(0)
+  }
+
+  def isSorted2[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    def go(i: Int): Boolean =
+      if (i == as.length - 1) true // if it got so far, the whole array is sorted
+      else if (gt(as(i), as(i + 1))) go(i + 1)
+      else false
+
+    go(0)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val as = Array(4, 3, 2, 1)
+    println(isSorted(as, (a: Int, b: Int) => a > b))
+    println(isSorted2(as, (a: Int, b: Int) => a > b))
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
 
-  def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
     (b: B) => f(a, b)
+
+  def partial2[A, B, C](a: A, f: (A, B) => C): B => C =
+    b => f(a, b)
+
+  def partial3[A, B, C](a: A, f: (A, B) => C): B => C =
+    f(a, _)
 
   // Exercise 3: Implement `curry`.
 
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
-  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    a => (b: B) => f(a, b)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -173,6 +256,12 @@ object PolymorphicFunctions {
 
   // Exercise 5: Implement `compose`
 
-  def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
+
+  def compose2[A, B, C](f: B => C, g: A => B): A => C =
+    f compose g
+
+  def compose3[A, B, C](f: B => C, g: A => B): A => C =
+    g andThen f
 }
